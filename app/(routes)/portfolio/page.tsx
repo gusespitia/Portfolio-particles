7e04ed013cbd6ce99b6afdec46c6dcdb7fbaeacd
Gustavo Espitia <146354509+gusespitia@users.noreply.github.com>
2024-11-15 17:09:22 +0100

    new files added

--- app/(routes)/portfolio/page.tsx
+++ app/(routes)/portfolio/page.tsx
@@ -12,8 +12,7 @@ const PortfolioPage = () => {
     <AvatarPortfolio />
     <CircleImage />
     <div className="flex flex-col justify-center h-full">
-        <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5">Mis últimos <span className="font-bold text-secondary">trabajos realizados</span></h1>
-
+        <h1 className="text-xl leading-tight text-center md:text-3xl md:mb-5">Mis últimos <span className="font-bold text-secondary">trabajos realizados</span></h1>
         <div className="relative z-10 grid max-w-3xl gap-4 mx-auto sm:grid-cols-2 mt-4 md:grid-cols-3 lg:grid-cols-4">
             {dataPortfolio.map((data) => (
                 <PortfolioBox key={data.id} data={data} />
